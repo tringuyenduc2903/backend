3f0e3f5d2c372e434abc54eed51337305d939ae7
nguyen-duc-tri <nguyenductri2k3@gmail.com>
2024-09-02 09:47:41 +0700

    Setup Laravel/Breeze package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+            return sprintf(
+                '%s/password-reset/%s?email=%s',
+                config('app.frontend_url'),
+                $token,
+                $notifiable->getEmailForPasswordReset()
+            );
+        });
     }
 }
