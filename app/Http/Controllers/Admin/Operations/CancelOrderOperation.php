1510b48978eb1cecc9f47d2ea0daa504258ab1d0
nguyen-duc-tri <nguyenductri2k3@gmail.com>
2024-09-22 00:52:46 +0700

    Add cancel order to Order CRUD

---
+++ app/Http/Controllers/Admin/Operations/CancelOrderOperation.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Operations;
+
+use App\Enums\OrderStatus;
+use App\Models\Order;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Route;
+
+trait CancelOrderOperation
+{
+    /**
+     * Define which routes are needed for this operation.
+     *
+     * @param  string  $segment  Name of the current entity (singular). Used as first URL segment.
+     * @param  string  $routeName  Prefix of the route name.
+     * @param  string  $controller  Name of the current CrudController.
+     */
+    protected function setupCancelOrderRoutes(string $segment, string $routeName, string $controller)
+    {
+        Route::delete($segment.'/{id}/cancel-order', [
+            'as' => $routeName.'.cancel',
+            'uses' => $controller.'@cancel',
+            'operation' => 'cancel_order',
+        ]);
+    }
+
+    protected function cancel(string $id): JsonResponse
+    {
+        $order = Order::findOrFail($id);
+
+        if ($order->status !== OrderStatus::TO_PAY) {
+            return response()->json([
+                'title' => '',
+                'description' => '',
+            ], 403);
+        }
+
+        $order->update([
+            'status' => OrderStatus::CANCELLED,
+        ]);
+
+        return response()->json([
+            'title' => '',
+            'description' => '',
+        ]);
+    }
+}
---
+++ resources/views/crud/buttons/order/cancel_order.blade.php
@@ -0,0 +1,67 @@
+@if ($crud->hasAccess('cancel_order', $entry))
+    <a bp-button="update" class="btn btn-sm btn-link"
+       href="javascript:void(0)" onclick="cancelOrder()">
+        <i class="la la-trash"></i> <span>{{ trans('Cancel order') }}</span>
+    </a>
+
+    <script>
+        if (typeof cancelOrder != 'function') {
+            function cancelOrder() {
+                // show confirm message
+                swal({
+                    title: "{{ trans('backpack::base.warning') }}",
+                    text: "{{ trans('Confirm cancellation of this order?') }}",
+                    icon: "warning",
+                    buttons: {
+                        cancel: {
+                            text: "{{ trans('backpack::crud.cancel') }}",
+                            value: null,
+                            visible: true,
+                            className: "bg-secondary",
+                            closeModal: true,
+                        },
+                        delete: {
+                            text: "{{ trans('backpack::crud.save') }}",
+                            value: true,
+                            visible: true,
+                            className: "bg-danger",
+                        }
+                    },
+                }).then((value) => {
+                    if (!value) {
+                        return
+                    }
+
+                    // submit an AJAX delete call
+                    $.ajax({
+                        url: "{{ url($crud->route) }}/{{ $entry->getKey() }}/cancel-order",
+                        type: 'DELETE',
+                        data: {
+                            entry: {{ $entry->getKey() }}
+                        },
+                        success: (result) => {
+                            console.log(result)
+
+                            // Show an alert with the result
+                            new Noty({
+                                type: "success",
+                                text: `<strong>${result.title}</strong><br>${result.description}`
+                            }).show();
+
+                            crud.checkedItems = [];
+                            crud.table.draw(false);
+                        },
+                        error: ({responseJSON}) => {
+                            // Show an alert with the result
+                            new Noty({
+                                type: "danger",
+                                text: `<strong>${responseJSON.title}</strong><br>${responseJSON.description}`
+                            }).show();
+                        },
+                    });
+                });
+            }
+        }
+    </script>
+
+@endif
