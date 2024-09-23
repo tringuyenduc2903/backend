5622dc1d2058ff032c2bbb9b479869e376229fa7
nguyen-duc-tri <nguyenductri2k3@gmail.com>
2024-09-23 11:41:28 +0700

    Fix issue missing Order Shipping Type Enum

---
+++ app/Enums/OrderShippingType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Enums;
+
+use Rexlabs\Enum\Enum;
+
+/**
+ * The OrderShippingType enum.
+ *
+ * @method static self PICKUP_AT_STORE()
+ * @method static self DOOR_TO_DOOR_DELIVERY()
+ */
+class OrderShippingType extends Enum
+{
+    const PICKUP_AT_STORE = 0;
+
+    const DOOR_TO_DOOR_DELIVERY = 1;
+
+    /**
+     * Retrieve a map of enum keys and values.
+     */
+    public static function map(): array
+    {
+        return [
+            static::PICKUP_AT_STORE => trans('Pickup at Store'),
+            static::DOOR_TO_DOOR_DELIVERY => trans('Door-to-door delivery'),
+        ];
+    }
+}
