d5bfe83c146f71bf7bc1c3b6d0aeeaaec9b3c120
nguyen-duc-tri <nguyenductri2k3@gmail.com>
2024-09-25 16:10:10 +0700

    Reformat code in Available Services Api

--- app/Actions/GiaoHangNhanh/ShippingOrder.php
+++ app/Actions/GiaoHangNhanh/ShippingOrder.php
@@ -6,6 +6,7 @@ use App\Models\District;
 use App\Models\Ward;
 use Exception;
 use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
@@ -18,7 +19,7 @@ trait ShippingOrder
     public function fee(array $data): array
     {
         $data['shop_id'] = current_store();
-        $data['service_type_id'] = 2;
+        $data['service_id'] = $this->getServiceId($data['to_district_id'], $data['weight']);
 
         handle_validate_failure(
             $validator = Validator::make($data, [
@@ -50,6 +51,40 @@ trait ShippingOrder
 
         handle_ghn_api(
             $response = $this->http->post('shipping-order/fee', $validator->validated())
+        );
+
+        return $response->json('data');
+    }
+
+    protected function getServiceId(int $to_district_id, int $weight): int
+    {
+        $services = \App\Facades\GHNv2Cache::availableServices([
+            'to_district' => $to_district_id,
+        ]);
+        $service = $weight < 20000 ? Arr::first($services) : Arr::last($services);
+
+        return $service['service_id'];
+    }
+
+    /**
+     * @throws ConnectionException
+     * @throws Exception
+     */
+    public function availableServices(array $data): array
+    {
+        $data['shop_id'] = current_store();
+        $data['from_district'] = current_store_district();
+
+        handle_validate_failure(
+            $validator = Validator::make($data, [
+                'shop_id' => ['required', 'integer'],
+                'from_district' => ['sometimes', 'integer', Rule::exists(District::class, 'ghn_id')],
+                'to_district' => ['required', 'integer', Rule::exists(District::class, 'ghn_id')],
+            ])
+        );
+
+        handle_ghn_api(
+            $response = $this->http->post('shipping-order/available-services', $validator->validated())
         );
 
         return $response->json('data');
--- app/Actions/GiaoHangNhanh/ShippingOrderCache.php
+++ app/Actions/GiaoHangNhanh/ShippingOrderCache.php
@@ -8,12 +8,23 @@ trait ShippingOrderCache
 {
     private int $fee_time = 300;
 
+    private int $available_services_time = 900;
+
     public function fee(array $data): array
     {
         return handle_cache(
             fn (): array => GHNv2::fee($data),
             sprintf('%s_%s_%s', __CLASS__, __METHOD__, json_encode($data)),
             $this->fee_time
+        );
+    }
+
+    public function availableServices(array $data): array
+    {
+        return handle_cache(
+            fn (): array => GHNv2::availableServices($data),
+            sprintf('%s_%s_%s', __CLASS__, __METHOD__, json_encode($data)),
+            $this->available_services_time
         );
     }
 }
